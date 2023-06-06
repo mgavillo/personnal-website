633bc4b2c1eaa71adff029abdbd7d7256d680de3
mgavillo <marie.gavillon@gmail.com>
2023-06-07 00:43:26 +0100

    adding domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images : {
+        remotePatterns: [
+            {
+                hostname:"phztnztrdvdbziposzlf.supabase.co"
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
