9677b0694f8c193caff8e3b5aeb1be7e31894e24
mgavillo <marie.gavillon@gmail.com>
2023-06-06 22:55:34 +0100

    polish config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        300: "#031531",
+        "dark-blue": "#030921",
+        "neon-blue": "#00FFFF",
+        "neon-pink": "#FF00E5",
+        "neon-green": "#00FF19",
+        "blue": "#0021FF",
+        "violet": "#3C0398",
+      },
+      boxShadow: {
+        box: "0px 0px 20px 4px #0038FF",
+        "box-xs": "0px 0px 7px 2px #0038FF",
+      },
+      height:{
+        'screen-no-header': 'calc(100vh -64px)',
+      }
     },
   },
-  plugins: [],
+  plugins: []
 }
