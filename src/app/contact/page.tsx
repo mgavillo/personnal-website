0592b9121d2d20363688d129657726b15836373d
mgavillo <marie.gavillon@gmail.com>
2023-06-06 23:58:57 +0100

    contact first archi

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+export default function Contact(){
+    return( 
+        <div className="w-full h-screen flex flex-col justify-center">
+            <h1 className="">Contact me</h1>
+            <h2>Have a project ?</h2>
+            <h3>What can I help you for ? </h3>
+            <div className="flex flex-col">
+                <p>Web development</p>
+                <p>UI/UX design</p>
+                <p>Illustration</p>
+                <p>3D</p>
+            </div>
+            <div></div>
+        </div>
+    )
+}
