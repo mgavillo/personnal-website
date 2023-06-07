029524588935483b77b2c7707e0fb8ccb457e591
mgavillo <marie.gavillon@gmail.com>
2023-06-07 17:33:41 +0100

    add socials

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import { SiInstagram, SiTwitter } from "react-icons/si";
 
 export default function Header() {
-
   return (
-    <header className="w-screen h-16 text-2xl fixed z-50 backdrop-blur-lg bg-dark-blue/50 flex items-center justify-center gap-12">
-      <Link href="/">Home</Link>
-      <Link href="/gallery">Gallery</Link>
-      <Link href="/contact">Contact</Link>
+    <header className="w-screen h-16 text-2xl fixed z-50 backdrop-blur-lg bg-dark-blue/50 flex justify-between p-4">
+      <div className="w-36"></div>
+      <div className="flex flex-row gap-8 h-full items-center">
+        <Link href="/">Home</Link>
+        <Link href="/gallery">Gallery</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+      <div className="flex flex-row gap-4 h-full items-center w-36 justify-end">
+        <div className="border border-transparent hover:border-white rounded-lg p-3 cursor-pointer">
+          <SiInstagram size={25} className=""/>
+        </div>
+        <div className="border border-transparent hover:border-white rounded-lg p-3 cursor-pointer">
+          <SiTwitter />
+        </div>
+      </div>
     </header>
   );
 }
