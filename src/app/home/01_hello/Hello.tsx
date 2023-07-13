08833db8394f02270567589694dcfa9f9c68a42c
mgavillo <marie.gavillon@gmail.com>
2023-07-13 21:19:14 +0100

    test better speed with suspense

--- src/app/home/01_hello/Hello.tsx
+++ src/app/home/01_hello/Hello.tsx
@@ -1,13 +1,11 @@
-"use client";
+import { Suspense } from "react";
 import Waves from "./Waves";
 export default function Hello() {
-
-
-
-
   return (
     <section className="w-full h-screen relative  mt-0">
-      <Waves/>
+      <Suspense fallback="">
+        <Waves />
+      </Suspense>
       <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-b from-transparent via-dark-blue to-transparent -mb-48 z-40 pointer-events-none h-96"></div>
     </section>
   );
--- src/app/home/01_hello/Waves.tsx
+++ src/app/home/01_hello/Waves.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Effects,
   MeshReflectorMaterial,
