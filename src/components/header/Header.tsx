15fb46a6865a7c88e302c396445ecc8734919a3e
mgavillo <marie.gavillon@gmail.com>
2023-08-23 21:59:02 +0100

    respnsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,6 +13,7 @@ export default function Header() {
   const [scrollDirection, setScrollDirection] = useState<"down" | "up">("up");
   const pathName = usePathname();
   const paths = ["home", "gallery", "contact"];
+
   function onScroll(e: any) {
     const w = e.currentTarget;
     if (y.current == null) return;
@@ -32,9 +33,8 @@ export default function Header() {
     <header
       className={` px-4 w-screen h-16 text-xl font-semibold fixed z-50 backdrop-blur-lg bg-dark-blue/50 flex justify-between top-0 transition-all border-b border-white/10 ${
         scrollDirection == "down" ? " -translate-y-20" : ""
-      }`}
-    >
-      <div className="w-36"></div>
+      }`}>
+      <div className="w-36 hidden md:flex"></div>
       <div className="flex flex-row h-full items-center">
         {paths.map((el, index) => {
           const currentPath = index == 0 ? "/" : "/" + el;
@@ -42,48 +42,35 @@ export default function Header() {
             <Link
               key={index}
               href={currentPath}
-              className="relative w-36 py-2 h-full flex items-center justify-center text-white/70 hover:text-white transition-all"
+              className="relative w-24  md:w-36 py-2 h-full flex items-center justify-center text-white/70 hover:text-white transition-all"
               onMouseEnter={() => setHovered(index)}
-              onMouseLeave={() => setHovered(null)}
-            >
-              <h3
-                className={` capitalize ${
-                  pathName == currentPath ? "text-white" : ""
-                }`}
-              >
-                {el}
-              </h3>
+              onMouseLeave={() => setHovered(null)}>
+              <h4 className={` capitalize ${pathName == currentPath ? "text-white" : ""}`}>{el}</h4>
               <div
                 className={`${
                   currentPath == pathName ? " w-1/2" : "w-0"
-                } h-[1px] bg-white absolute bottom-0 transition-all`}
-              ></div>
-              <HoveredSelector
-                hovered={hovered === index}
-                className="top-2 bottom-2"
-              />
+                } h-[1px] bg-white absolute bottom-0 transition-all`}></div>
+              <HoveredSelector hovered={hovered === index} className="top-2 bottom-2" />
             </Link>
           );
         })}
       </div>
-      <div className="flex flex-row h-full items-center justify-end">
+      <div className="hidden md:flex flex-row h-full items-center justify-end">
         <a
           href={"https://www.instagram.com/_swaggymarie"}
           target="_blank"
-          className="relative cursor-pointer h-full flex items-center justify-center w-16 transition-all text-white/70 hover:text-white"
+          className="relative cursor-pointer h-full flex items-center justify-center w-8 md:w-16 transition-all text-white/70 hover:text-white"
           onMouseEnter={() => setHovered(3)}
-          onMouseLeave={() => setHovered(null)}
-        >
+          onMouseLeave={() => setHovered(null)}>
           <SiInstagram className="text-2xl" />
           <HoveredSelector hovered={hovered === 3} className="top-2 bottom-2" />
         </a>
         <a
           href={"https://www.twitter.com/swagy_marie"}
           target="_blank"
-          className="relative cursor-pointer h-full flex items-center justify-center w-16 transition-all text-white/70 hover:text-white"
+          className="relative cursor-pointer h-full flex items-center justify-center w-8 md:w-16 transition-all text-white/70 hover:text-white"
           onMouseEnter={() => setHovered(4)}
-          onMouseLeave={() => setHovered(null)}
-        >
+          onMouseLeave={() => setHovered(null)}>
           <SiTwitter className="text-2xl" />
           <HoveredSelector hovered={hovered === 4} className="top-2 bottom-2" />
         </a>
