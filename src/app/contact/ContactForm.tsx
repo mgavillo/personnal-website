15470c8735402444098b31c15529c7f1cb7c3999
mgavillo <marie.gavillon@gmail.com>
2023-08-24 17:48:34 +0100

    responsive contact

--- src/app/contact/ContactForm.tsx
+++ src/app/contact/ContactForm.tsx
@@ -1,102 +1,77 @@
-
-import {TextArea, Input} from "@/components/inputs";
+import { TextArea, Input } from "@/components/inputs";
 import { FormEvent, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import SectionTitle from "@/components/SectionTitle";
 
+export default function ContactForm({ setSent }: { setSent: any }) {
+  const categories = ["Design", "Web development", "Deployment"];
+  const [selected, setSelected] = useState<boolean[]>(new Array(categories.length).fill(false));
+  const formRef = useRef<HTMLFormElement>(null);
+  function changeSelection(index: number) {
+    let array = [...selected];
+    array[index] = !array[index];
+    setSelected(array);
+  }
 
-export default function ContactForm({setSent}: {setSent: any}) {
-    const categories = ["Design", "Web development", "Deployment"];
-    const [selected, setSelected] = useState<boolean[]>(
-      new Array(categories.length).fill(false)
-    );
-    const formRef = useRef<HTMLFormElement>(null);
-    function changeSelection(index: number) {
-      let array = [...selected];
-      array[index] = !array[index];
-      setSelected(array);
-    }
-  
-    function sendEmail(e: React.FormEvent<HTMLFormElement>) {
-      e.preventDefault();
-      emailjs
-        .sendForm(
-          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-          formRef.current,
-          process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-        )
-        .then(
-          (result) => {
-            setSent(true)
-            console.log(result.text);
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-    }
-    
+  function sendEmail(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        formRef.current,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setSent(true);
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
+
   return (
-    <form
-      ref={formRef}
-      className=" flex flex-col gap-4 w-3/5 h-full bg-dark-blue/10 justify-around items-center p-24 pt-28 backdrop-blur-lg"
-      onSubmit={(e) => sendEmail(e)}
-    >
-      <div className="w-full">
-        <h2 className=" text-4xl">Have a project ?</h2>
-        <h1 className="mb-12 text-7xl">Contact me</h1>
-      </div>
-      <div className="flex flex-col gap-2 w-full">
-        <h3 className="">I'm interested in... </h3>
-        <div className="flex flex-row gap-2">
-          {categories.map((el, i) => (
-            <div
-              onClick={() => changeSelection(i)}
-              className={`relative bg-dark-blue border border-white/10 rounded-md text-center cursor-pointer hover:bg-transparent flex items-center justify-center px-4 py-2 hover:bg-gradient-to-r hover:from-blue/20 hover:to-neon-blue/30 ${
-                selected[i]
-                  ? "bg-gradient-to-r from-blue/60 to-neon-blue/60"
-                  : "text-gray-400"
-              }`}
-            >
-              {el}
-              <input
-                id={el}
-                type="checkbox"
-                checked={selected[i]}
-                className=" hidden"
-              />
-            </div>
-          ))}
+    <section className="py-24">
+      <form
+        ref={formRef}
+        className=" flex flex-col gap-4 w-full md:w-3/5 h-full bg-dark-blue/10 justify-around items-center backdrop-blur-lg"
+        onSubmit={(e) => sendEmail(e)}>
+        <div className="w-full flex flex-col gap-3">
+          <h3 className=" text-gray-400">Have a project ?</h3>
+          <span className="mb-12">
+            <SectionTitle text="Contact me" />
+          </span>
         </div>
-        <div className="flex flex-col gap-3 mt-4">
-          <Input
-            className="w-fill"
-            type="name"
-            label="Name"
-            placeHolder=""
-            name="user_name"
-          />
-          <Input
-            className="w-fill"
-            type="email"
-            label="Email address"
-            placeHolder=""
-            name="user_email"
-          />
-          <TextArea
-            label="Tell me more about it"
-            placeHolder=""
-            name="details"
-          />
-          <div className="w-full flex flex-col items-center"></div>
+        <div className="flex flex-col gap-2 w-full">
+          <p className="">I'm interested in... </p>
+          <div className="flex flex-row gap-2">
+            {categories.map((el, i) => (
+              <div
+                onClick={() => changeSelection(i)}
+                className={`relative bg-dark-blue border border-white/10 rounded-md text-center cursor-pointer hover:bg-transparent flex items-center justify-center text-xs md:text-base px-4 py-2 hover:bg-gradient-to-r hover:from-blue/20 hover:to-neon-blue/30 ${
+                  selected[i] ? "bg-gradient-to-r from-blue/60 to-neon-blue/60" : "text-gray-400"
+                }`}>
+                {el}
+                <input id={el} type="checkbox" checked={selected[i]} className=" hidden" />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col gap-3 mt-4">
+            <Input className="w-fill" type="name" label="Name" placeHolder="" name="user_name" />
+            <Input className="w-fill" type="email" label="Email address" placeHolder="" name="user_email" />
+            <TextArea label="Tell me more about it" placeHolder="" name="details" />
+            <div className="w-full flex flex-col items-center"></div>
+          </div>
         </div>
-      </div>
-      <button
-        type="submit"
-        className=" text-xl font-semibold px-4 py-3 rounded-md border border-white/10 w-fit bg-black hover:shadow-box-xs"
-      >
-        Submit
-      </button>
-    </form>
+        <button
+          type="submit"
+          className=" text-xl font-semibold px-4 py-3 rounded-md border border-white/10 w-fit bg-black hover:shadow-box-xs">
+          Submit
+        </button>
+      </form>
+    </section>
   );
 }
--- src/app/contact/EmailSent.tsx
+++ src/app/contact/EmailSent.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 import gif from "/public/logo.gif"
 export default function EmailSent() {
   return (
-    <div className="w-full flex h-full items-center justify-center flex-col gap-2 ">
+    <section className="items-center justify-center gap-4 ">
       <Image
         alt="gif of an eye blicking "
-        className="drop-shadow-[0_35px_35px_rgba(0,1,0,0.25)]"
+        className="drop-shadow-[0_35px_35px_rgba(0,1,0,0.25)] mb-12"
         src={gif}
         width={200}
         height={200}
 />
-      <h1 className="text-5xl  filter drop-shadow-md shadow-rose-600">Your message has been sent.</h1>
+      <h1 className="filter drop-shadow-md shadow-rose-600">Your message has been sent.</h1>
       <h3 className="text-white/60">
         Thank you for reaching out, I will come back to you soon!{" "}
       </h3>
-    </div>
+    </section>
   );
 }
