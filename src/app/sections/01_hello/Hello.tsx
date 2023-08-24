f58151beddbf781c35ef97fb213a9077b19984c5
mgavillo <marie.gavillon@gmail.com>
2023-08-24 23:39:42 +0100

    Perfomance | moved back html outside of 3D canvas

--- src/app/sections/01_hello/Hello.tsx
+++ src/app/sections/01_hello/Hello.tsx
@@ -1,10 +1,37 @@
 import { Suspense } from "react";
 import dynamic from "next/dynamic";
+import TypedLetters from "@/components/TypedLetters";
 const SpaceBg = dynamic(() => import("./SpaceBg"), { ssr: false });
+
+const subTexts = [
+  "write specifications",
+  "design figma mocks",
+  "develop your website",
+  "conceive data structures",
+  "manage databases",
+  "host and deploy",
+];
+
 
 export default function Hello() {
   return (
     <section className="p-0 relative overflow-hidden">
+      <div className="absolute inset-0 flex flex-col justify-center items-center">
+        <p>Hello, i'm Marie</p>
+        <h1 className="h-32 md:h-64 text-center bg-gradient-radial from-white from-30% to-blue/5 text-white text-opacity-50 bg-clip-text text-3xl md:text-8xl">
+          Developer and Designer
+        </h1>
+        <div className="text-left flex flex-row gap-x-2 gap-y-0 flex-wrap w-full justify-center">
+          <h3>I can help you</h3>
+          <h3>
+            <TypedLetters
+              texts={subTexts}
+              className="bg-gradient-to-r from-neon-pink to-neon-blue text-white/25 bg-clip-text relative font-bold"
+            />
+          </h3>
+        </div>
+      </div>
+      
       <Suspense fallback="">
         <SpaceBg />
       </Suspense>
--- src/app/sections/01_hello/SpaceBg.tsx
+++ src/app/sections/01_hello/SpaceBg.tsx
@@ -1,23 +1,12 @@
 "use client";
 
-import {Html} from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import { Bloom, EffectComposer} from "@react-three/postprocessing";
 import { useScroll, useTransform } from "framer-motion";
 import useOnScreen from "@/lib/useOnScreen";
-import TypedLetters from "@/components/TypedLetters";
 
-const subTexts = [
-  "write specifications",
-  "design figma mocks",
-  "develop your website",
-  "conceive data structures",
-  "manage databases",
-  "host and deploy",
-];
-
-function Text() {
+function Lights() {
   const { scrollYProgress } = useScroll();
   const xposition = useTransform(scrollYProgress, [0, 1], [0, -20]);
   useFrame(({ clock, camera }) => {
@@ -26,28 +15,6 @@ function Text() {
     // camera.lookAt(lookat);
   });
 
-  return (
-    <Html as="div" center position={[0, 0, 1200]} receiveShadow castShadow className="w-screen">
-      <div className="flex flex-col justify-start items-center">
-        <p>Hello, i'm Marie</p>
-        <h1 className="h-32 md:h-64 text-center bg-gradient-radial from-white from-30% to-blue/5 text-white text-opacity-50 bg-clip-text text-3xl md:text-8xl">
-          Developer and Designer
-        </h1>
-        <div className="text-left flex flex-row gap-x-2 gap-y-0 flex-wrap w-full justify-center">
-          <h3>I can help you</h3>
-          <h3>
-            <TypedLetters
-              texts={subTexts}
-              className="bg-gradient-to-r from-neon-pink to-neon-blue text-white/25 bg-clip-text relative font-bold"
-            />
-          </h3>
-        </div>
-      </div>
-    </Html>
-  );
-}
-
-function Lights() {
   return (
     <>
       <directionalLight intensity={30} color="#FF00E5" rotation={[0, 0, 1.60254]} position={[0, 0, 300]} />
@@ -78,7 +45,7 @@ export default function SpaceBg() {
       ref={canvasRef}
       frameloop={isOnScreen ? "always" : "never"}
       camera={{ fov: 25, far: 1000, near: 0.1, position: [0, 0, -100] }}>
-      <Text />
+      {/* <Text /> */}
       <Lights />
       <group position={[isMobile ? 17 : 50, 24, 100.29852]}>
         <mesh castShadow receiveShadow scale={0.40021}>
