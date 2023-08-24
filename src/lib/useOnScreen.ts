f63d630b1c9ac760364a8bdf40ddd27371e36589
mgavillo <marie.gavillon@gmail.com>
2023-08-25 00:17:36 +0100

    add type for obseerver

--- src/lib/useOnScreen.ts
+++ src/lib/useOnScreen.ts
@@ -1,23 +1,22 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { MutableRefObject, useEffect, useMemo, useState } from "react";
 
-export default function useOnScreen(ref: any) {
+export default function useOnScreen(ref: MutableRefObject<any>) {
   const [isIntersecting, setIntersecting] = useState(false);
   const [isInTab, setIsInTab] = useState(true);
-  let observer:IntersectionObserver;
-  useEffect(() => {
-    observer = new IntersectionObserver(([entry]) =>
-    setIntersecting(entry.isIntersecting)
-  )
-  }, [ref])
+  let observer: IntersectionObserver;
 
   useEffect(() => {
+    observer = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting));
+  }, [ref]);
+
+  useEffect(() => {
+    if (!ref.current) return;
     function handleHidden() {
       if (document.hidden) setIsInTab(false);
       else setIsInTab(true);
     }
-
     observer?.observe(ref.current);
     window.addEventListener("visibilitychange", handleHidden);
     return () => {
