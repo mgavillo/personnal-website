bb709abdc2aa36be08c30691d405499576e11fc6
mgavillo <marie.gavillon@gmail.com>
2023-08-24 21:37:57 +0100

    update import after file clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image";
-import Hello from "./home/01_hello/Hello";
-import Whoami from "./home/02_whoami/Whoami";
-import Dual3D from "./home/04_dual3D/Dual3D";
+import Hello from "./sections/01_hello/Hello";
+import Whoami from "./sections/02_whoami/Whoami";
 import Waves3 from "@/components/backgrounds/Waves3";
-import Skills from "./home/03_skills/Skills";
+import Skills from "./sections/03_skills/Skills";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-between h-fit overflow-hidden md:gap-24">
+    <main className="relative flex min-h-screen flex-col items-center justify-between h-fit overflow-hidden gap-2 md:gap-24">
       <Hello />
       <Whoami />
       <Skills />
-      <div className="-z-10 absolute -right-32 md:right-0 -scale-x-100 w-[100vh] h-[300vh] top-[100vh]">
+      <div className="-z-20 absolute -right-32 md:right-0 -scale-x-100 w-[100vh] h-[300vh] top-[100vh]">
         <div className="relative">
           <div className="absolute inset-0 bg-gradient-to-t from-dark-blue via-transparent to-dark-blue" />
           <Waves3 />
