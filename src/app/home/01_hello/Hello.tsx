ab1942d1f46af6ee87256c755fa23103f6d88af5
mgavillo <marie.gavillon@gmail.com>
2023-08-24 20:13:43 +0100

    clean component and better responsiveness

--- src/app/home/01_hello/Hello.tsx
+++ src/app/home/01_hello/Hello.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import Waves from "./Waves";
+import SpaceBg from "./SpaceBg";
 export default function Hello() {
   return (
     <section className="p-0 relative overflow-hidden">
       <Suspense fallback="">
-        <Waves />
+        <SpaceBg/>
       </Suspense>
       <div className="absolute bottom-0 left-0 right-0 z-40 bg-gradient-to-b from-transparent via-dark-blue to-transparent -mb-48 pointer-events-none h-96"></div>
     </section>
--- src/app/home/01_hello/SpaceBg.tsx
+++ src/app/home/01_hello/SpaceBg.tsx
@@ -1,37 +1,13 @@
-"use client"
+"use client";
 
-import {
-  Effects,
-  MeshReflectorMaterial,
-  MeshTransmissionMaterial,
-  OrbitControls,
-  PerspectiveCamera,
-  Stars,
-  Html,
-} from "@react-three/drei";
-import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
-import { vertex, fragment } from "./shaders";
-import { useRef } from "react";
-import { BufferGeometry, Material, Mesh, MeshPhongMaterial, NormalBufferAttributes, PlaneGeometry } from "three";
-import * as THREE from "three";
-import { UnrealBloomPass } from "three-stdlib";
-import {
-  Autofocus,
-  Bloom,
-  DepthOfField,
-  DotScreen,
-  EffectComposer,
-  Noise,
-  Vignette,
-} from "@react-three/postprocessing";
-extend({ UnrealBloomPass });
-import { BlendFunction } from "postprocessing";
+import {Html} from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import { Bloom, EffectComposer} from "@react-three/postprocessing";
 import { useScroll, useTransform } from "framer-motion";
 import useOnScreen from "@/lib/useOnScreen";
 import TypedLetters from "@/components/TypedLetters";
 
-const vector = [0, 0, 0];
-const lookat = new THREE.Vector3(100, 30, 500);
 const subTexts = [
   "write specifications",
   "design figma mocks",
@@ -54,39 +30,57 @@ function Text() {
     <Html as="div" center position={[0, 0, 1200]} receiveShadow castShadow className="w-screen">
       <div className="flex flex-col justify-start items-center">
         <p>Hello, i'm Marie</p>
-        <h1 className=" w-[1200px] h-32 text-center bg-gradient-radial from-white from-30% to-blue/5 text-white text-opacity-50 bg-clip-text">
-          Developer and designer
+        <h1 className="h-32 md:h-64 text-center bg-gradient-radial from-white from-30% to-blue/5 text-white text-opacity-50 bg-clip-text text-3xl md:text-8xl">
+          Developer and Designer
         </h1>
-        <h3 className="text-left flex flex-row gap-2">
-          I can help you
-          <TypedLetters
-            texts={subTexts}
-            className="bg-gradient-to-r from-neon-pink to-neon-blue text-white/25 bg-clip-text relative font-bold"
-          />
-        </h3>
+        <div className="text-left flex flex-row gap-x-2 gap-y-0 flex-wrap w-full justify-center">
+          <h3>I can help you</h3>
+          <h3>
+            <TypedLetters
+              texts={subTexts}
+              className="bg-gradient-to-r from-neon-pink to-neon-blue text-white/25 bg-clip-text relative font-bold"
+            />
+          </h3>
+        </div>
       </div>
     </Html>
   );
 }
 
-export default function Waves() {
+function Lights() {
+  return (
+    <>
+      <directionalLight intensity={30} color="#FF00E5" rotation={[0, 0, 1.60254]} position={[0, 0, 300]} />
+      <directionalLight intensity={10} color="#0000FF" rotation={[0, 0, 1.60254]} position={[0, 50, 300]} />
+      <directionalLight intensity={1} color="#6a00ff" position={[0, 20, -50]} rotation={[0, 3, 0]} />
+      <ambientLight color="#2809DA" />
+    </>
+  );
+}
+
+export default function SpaceBg() {
   const canvasRef = useRef(null);
-  const uniforms = {
-    center: { value: "co" },
-  };
   const isOnScreen = useOnScreen(canvasRef);
+  const [isMobile, setIsMobile] = useState<boolean>(typeof window !== "undefined" && window.innerWidth <= 767.98);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(typeof window !== "undefined" && window.innerWidth <= 767.98);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <Canvas
       ref={canvasRef}
       frameloop={isOnScreen ? "always" : "never"}
       camera={{ fov: 25, far: 1000, near: 0.1, position: [0, 0, -100] }}>
-      <directionalLight intensity={30} color="#FF00E5" rotation={[0, 0, 1.60254]} position={[0, 0, 300]} />
-      <directionalLight intensity={10} color="#0000FF" rotation={[0, 0, 1.60254]} position={[0, 50, 300]} />
-      <directionalLight intensity={1} color="#6a00ff" position={[0, 20, -50]} rotation={[0, 3, 0]} />
       <Text />
-      <ambientLight color="#2809DA" />
-      <group position={[50, 24, 100.29852]}>
+      <Lights />
+      <group position={[isMobile ? 17 : 50, 24, 100.29852]}>
         <mesh castShadow receiveShadow scale={0.40021}>
           <sphereGeometry args={[3, 32, 16]} />
           <meshPhongMaterial color="#2809DA" />
@@ -96,7 +90,7 @@ export default function Waves() {
           <meshPhongMaterial color="#2809da5a" />
         </mesh>
       </group>
-      <mesh castShadow receiveShadow position={[-70, 23, 120]} scale={0.91328}>
+      <mesh castShadow receiveShadow position={[isMobile ? -20 : -70, 23, 120]} scale={0.91328}>
         <sphereGeometry args={[4, 32, 16]} />
         <meshPhongMaterial color="#2809da5a" />
       </mesh>
@@ -111,11 +105,7 @@ export default function Waves() {
       </mesh>
       <EffectComposer>
         <Bloom mipmapBlur luminanceThreshold={0.2} />
-        {/* <Autofocus /> */}
       </EffectComposer>
-      {/* <Effects> */}
-      {/* </Effects> */}
-      {/* <OrbitControls /> */}
     </Canvas>
   );
 }
